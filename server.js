b6c842943576c3bcf884e11efa77d095fdcb897a
Jessica Vinluan <jessicavinluan@Jessicas-MacBook-Air.local>
2022-05-21 21:28:31 -0400

    updating

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.json());
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 // app.use(express.static(path.join(__dirname, 'build')));
 
+//routes
+
 // Put API routes here, before the "catch all" route
 
 // The following "catch all" route (note the *)is necessary
